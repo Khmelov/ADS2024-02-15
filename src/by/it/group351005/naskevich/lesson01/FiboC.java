cf192fac723f8aaccf5412361913af60a4faa39c
Exponenta <the.exponenta@gmail.com>
2024-02-20 18:25:13 +0300

    Lesson01, task C

--- src/by/it/group351005/naskevich/lesson01/FiboC.java
+++ src/by/it/group351005/naskevich/lesson01/FiboC.java
@@ -1,5 +1,7 @@
 package by.it.group351005.naskevich.lesson01;
 
+import java.util.ArrayList;
+
 /*
  * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
  * необходимо найти остаток от деления n-го числа Фибоначчи на m.
@@ -16,16 +18,42 @@ public class FiboC {
 
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
-        int n = 10;
-        int m = 2;
+        int n = 21;
+        int m = 4;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
     long fasterC(long n, int m) {
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return 0L;
+        if (n < 2) {
+            return n;
+        }
+
+        ArrayList<Long> period = new ArrayList<>();
+        period.add(0l);
+        period.add(1l);
+
+        int i = 2;
+        int periodPos = 0;
+        int periodEnd = 1;
+        long nextFib = 0;
+        while(periodPos != periodEnd && i <= n) {
+            nextFib = (period.get(i - 1) + period.get(i - 2)) % m;
+            period.add(nextFib);
+
+            if (nextFib == period.get(periodPos))
+                periodPos++;
+            else {
+                periodPos = 0;
+                periodEnd = i;
+            }
+
+            i++;
+        }
+
+        if (i > n)
+            return nextFib;
+
+        return period.get((int)(n % (periodEnd + 1)));
     }
 
 

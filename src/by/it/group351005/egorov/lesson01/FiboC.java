e69e31ec41eb0ad4a37b423606ab2c17abb11f77
Aleeg0 <Sankafoxe@gmail.com>
2024-02-20 19:23:19 +0300

    task C was solved

--- src/by/it/group351005/egorov/lesson01/FiboC.java
+++ src/by/it/group351005/egorov/lesson01/FiboC.java
@@ -6,6 +6,9 @@ package by.it.group351005.egorov.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -22,10 +25,22 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return 0L;
+        List<Integer> list = new ArrayList<>();
+        int temp = 0;
+        int n0 = 0;
+        int n1 = 1;
+        boolean isNotFound = true;
+        list.add(n0);
+        list.add(n1);
+        while (isNotFound){
+            temp = (n0 + n1) % m;
+            n0 = n1;
+            n1 = temp;
+            list.add(temp);
+            if (list.get(list.size() - 1) == 1 && list.get(list.size() - 2) == 0)
+                isNotFound = false;
+        }
+        return list.get((int)(n % (list.size() - 2)));
     }
 
 

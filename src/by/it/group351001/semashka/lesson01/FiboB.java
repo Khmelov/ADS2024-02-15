0dc5fb9a1374f530b86e9b6a53b089d11d857ee0
ivan <ivansemashko05@gmail.com>
2024-02-22 01:34:36 +0300

    lesson_1

--- src/by/it/group351001/semashka/lesson01/FiboB.java
+++ src/by/it/group351001/semashka/lesson01/FiboB.java
@@ -28,7 +28,7 @@ public class FiboB {
         BigInteger[] fib = new BigInteger[n+1];
         fib[0] = BigInteger.ZERO;
         fib[1] = BigInteger.ONE;
-        for (int i = 2; i<=n; i++) {
+        for (int i = 2; i <= n; i++) {
             fib[i] = fib[i - 1].add(fib[i - 2]);
         }
         return fib[n];
--- src/by/it/group351001/semashka/lesson01/FiboC.java
+++ src/by/it/group351001/semashka/lesson01/FiboC.java
@@ -33,7 +33,7 @@ public class FiboC {
         if (n == 0) return 0;
         else if (n == 1) return 1;
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i<=n; i++) {
             long temp = (previous + current) % m;
             previous = current;
             current = temp;

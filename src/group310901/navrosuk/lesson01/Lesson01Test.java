9faac285f5972c7c29136445957eea872a71fb6c
WhiteVirion <mr.navrosyuk@mail.ru>
2024-02-22 02:48:01 +0300

    Lesson1 A, B, C

--- src/group310901/navrosuk/lesson01/Lesson01Test.java
+++ src/group310901/navrosuk/lesson01/Lesson01Test.java
@@ -1,4 +1,4 @@
-package by.it.a_khmelev.lesson01;
+package group310901.navrosuk.lesson01;
 
 import org.junit.Test;
 
@@ -7,7 +7,7 @@ import java.math.BigInteger;
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("all")
-public class    Lesson01Test {
+public class Lesson01Test {
     /*
     для прохождения тестов создайте JUnit-конфигурацию на свой пакет:
     Поля:

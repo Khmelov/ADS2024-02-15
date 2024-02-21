dbf30581119509540bce5614636eefc38a1fd69a
ACER <romusan1@yandex.by>
2024-02-21 23:00:38 +0300

    Lesson1 A

--- src/group351002/pisarik/lesson01/FiboA.java
+++ src/group351002/pisarik/lesson01/FiboA.java
@@ -40,10 +40,7 @@ public class FiboA {
 
     BigInteger slowA(Integer n) {
 
-        //рекурсия
-        //здесь нужно реализовать вариант без ограничения на размер числа,
-        //в котором код совпадает с математическим определением чисел Фибоначчи
-        //время O(2^n)
+
         if (n==0) return BigInteger.ZERO;
         if (n==1) return BigInteger.ONE;
         return slowA(n - 1).add(slowA(n -2));

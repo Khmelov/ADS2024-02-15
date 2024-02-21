80f78b0b2e854d6a37e7189fa84812b465224667
artonka <janpeakland@gmail.com>
2024-02-22 01:09:34 +0300

    Lesson1 В

--- src/by/it/mikhovich/lesson01/FiboB.java
+++ src/by/it/mikhovich/lesson01/FiboB.java
@@ -25,7 +25,13 @@ public class FiboB {
 
     BigInteger fastB(Integer n) {
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        return BigInteger.ZERO;
+        BigInteger[] fib = new BigInteger[n + 1];
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1].add(fib[i - 2]);
+        }
+        return fib[n];
     }
 
 }

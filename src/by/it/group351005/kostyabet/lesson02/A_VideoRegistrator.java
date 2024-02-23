8bb41995bc8b98d57481022bb6d401f37efa7876
Kostya <betenya.kostya@mail.ru>
2024-02-23 19:08:17 +0300

    A_VideoRegistrator complete

--- src/by/it/group351005/kostyabet/lesson02/A_VideoRegistrator.java
+++ src/by/it/group351005/kostyabet/lesson02/A_VideoRegistrator.java
@@ -1,6 +1,7 @@
 package by.it.group351005.kostyabet.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 Даны события events
@@ -15,7 +16,7 @@ public class A_VideoRegistrator {
     public static void main(String[] args) {
         A_VideoRegistrator instance=new A_VideoRegistrator();
         double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длиной сеанса 1
         System.out.println(starts);                            //покажем моменты старта
     }
     //модификаторы доступа опущены для возможности тестирования
@@ -36,8 +37,16 @@ public class A_VideoRegistrator {
         //вычислим момент окончания работы видеокамеры
         //и теперь пропустим все покрываемые события
         //за время до конца работы, увеличивая индекс
-
-
+        Arrays.sort(events);
+        double curentTime = 0;
+        while (i < events.length){
+            if (curentTime >= events[i]) i++;
+            else
+            {
+                result.add(events[i]);
+                curentTime = events[i] + workDuration;
+            }
+        }
 
         return result;                        //вернем итог
     }

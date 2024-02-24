8a9bd59633edd718c6dd2933c007a701261f6bdb
fluffy11 <d.matsiushenka@gmail.com>
2024-02-24 19:40:41 +0300

    Lesson 2 Task A, B, C

--- src/by/it/Group351001/anton_matsiushenka/lesson02/B_Sheduler.java
+++ src/by/it/Group351001/anton_matsiushenka/lesson02/B_Sheduler.java
@@ -1,6 +1,8 @@
 package by.it.group351001.anton_matsiushenka.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 /*
 Даны интервальные события events
@@ -22,21 +24,22 @@ public class B_Sheduler {
 
         @Override
         public String toString() {
-            return "("+ start +":" + stop + ")";
+            return "(" + start + ":" + stop + ")";
         }
     }
 
+
     public static void main(String[] args) {
         B_Sheduler instance = new B_Sheduler();
-        Event[] events = {  new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
-                new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
-                new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
-                new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
-                new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
-                new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
+        Event[] events = {new Event(0, 3), new Event(0, 1), new Event(1, 2), new Event(3, 5),
+                new Event(1, 3), new Event(1, 3), new Event(1, 3), new Event(3, 6),
+                new Event(2, 7), new Event(2, 3), new Event(2, 7), new Event(7, 9),
+                new Event(3, 5), new Event(2, 4), new Event(2, 3), new Event(3, 7),
+                new Event(4, 5), new Event(6, 7), new Event(6, 9), new Event(7, 9),
+                new Event(8, 9), new Event(4, 6), new Event(8, 10), new Event(7, 10)
         };
 
-        List<Event> starts = instance.calcStartTimes(events,0,10);  //рассчитаем оптимальное заполнение аудитории
+        List<Event> starts = instance.calcStartTimes(events, 0, 10);  //рассчитаем оптимальное заполнение аудитории
         System.out.println(starts);                                 //покажем рассчитанный график занятий
     }
 
@@ -47,13 +50,14 @@ public class B_Sheduler {
         //Начало и конец событий могут совпадать.
         List<Event> result;
         result = new ArrayList<>();
-        //ваше решение.
-
-
-
-
-
-
-        return result;          //вернем итог
+        Arrays.sort(events, Comparator.comparingInt(event -> event.stop));
+        int currentEnd = Integer.MIN_VALUE;
+        for (Event event : events) {
+            if (event.start >= currentEnd && event.start >= from && event.stop <= to) {
+                result.add(event);
+                currentEnd = event.stop;
+            }
+        }
+        return result;
     }
 }
--- src/by/it/Group351001/anton_matsiushenka/lesson02/C_GreedyKnapsack.java
+++ src/by/it/Group351001/anton_matsiushenka/lesson02/C_GreedyKnapsack.java
@@ -12,8 +12,11 @@ package by.it.group351001.anton_matsiushenka.lesson02;
 Необходимо собрать наиболее дорогой вариант рюкзака для этого объема
 Предметы можно резать на кусочки (т.е. алгоритм будет жадным)
  */
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class C_GreedyKnapsack {
@@ -28,17 +31,11 @@ public class C_GreedyKnapsack {
 
         @Override
         public String toString() {
-            return "Item{" +
-                    "cost=" + cost +
-                    ", weight=" + weight +
-                    '}';
+            return "Item{" + "cost=" + cost + ", weight=" + weight + '}';
         }
 
         @Override
         public int compareTo(Item o) {
-            //тут может быть ваш компаратор
-
-
             return 0;
         }
     }
@@ -52,35 +49,34 @@ public class C_GreedyKnapsack {
             items[i] = new Item(input.nextInt(), input.nextInt());
         }
         //покажем предметы
-        for (Item item:items) {
+        for (Item item : items) {
             System.out.println(item);
         }
-        System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n",n,W);
-
-        //тут необходимо реализовать решение задачи
-        //итогом является максимально воможная стоимость вещей в рюкзаке
-        //вещи можно резать на кусочки (непрерывный рюкзак)
+        System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n", n, W);
         double result = 0;
-        //тут реализуйте алгоритм сбора рюкзака
-        //будет особенно хорошо, если с собственной сортировкой
-        //кроме того, можете описать свой компаратор в классе Item
+        Arrays.sort(items, Comparator.comparingDouble(item -> (double) item.weight / item.cost));
+        System.out.println(Arrays.toString(items));
+        for (Item item : items) {
+            if (W > item.weight) {
+                result += item.cost;
+                W -= item.weight;
+            } else {
+                result += ((double) item.cost / item.weight / W) * item.cost;
+                break;
+            }
+        }
 
-        //ваше решение.
 
-
-
-
-
-        System.out.printf("Удалось собрать рюкзак на сумму %f\n",result);
+        System.out.printf("Удалось собрать рюкзак на сумму %f\n", result);
         return result;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         long startTime = System.currentTimeMillis();
-        String root=System.getProperty("user.dir")+"/src/";
-        File f=new File(root+"by/it/a_khmelev/lesson02/greedyKnapsack.txt");
-        double costFinal=new C_GreedyKnapsack().calc(f);
+        String root = System.getProperty("user.dir") + "/src/";
+        File f = new File(root + "by/it/a_khmelev/lesson02/greedyKnapsack.txt");
+        double costFinal = new C_GreedyKnapsack().calc(f);
         long finishTime = System.currentTimeMillis();
-        System.out.printf("Общая стоимость %f (время %d)",costFinal,finishTime - startTime);
+        System.out.printf("Общая стоимость %f (время %d)", costFinal, finishTime - startTime);
     }
 }

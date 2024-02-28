0a3f3e239a85a03173ecb10b9b6ee73d82ecdc96
anothernpc <gavgavgavmeow2@gmail.com>
2024-02-28 21:45:07 +0300

    lesson 1

--- src/by/it/group351003/lutai/lesson01/FiboB.java
+++ src/by/it/group351003/lutai/lesson01/FiboB.java
@@ -25,16 +25,17 @@ public class FiboB {
 
     BigInteger fastB(Integer n) {
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        BigInteger[] fiboarr;
-        if (n == 0) return BigInteger.ZERO;
-        if (n == 1) return BigInteger.ONE;
+        if (n == 0)
+            return BigInteger.ZERO;
+        if (n == 1)
+            return BigInteger.ONE;
         n++;
-        fiboarr = new BigInteger[n];
-        for (int i = 2; i < n; i++){
-            fiboarr[i] = fiboarr[i-1].add(fiboarr[i-2]);
-        }
-        return fiboarr[n-1];
+        BigInteger arr[] = new BigInteger[n];
+        arr[0] = BigInteger.ZERO;
+        arr[1] = BigInteger.ONE;
+        for (int i = 2; i < n; i++)
+            arr[i] = arr[i - 1].add(arr[i - 2]);
+        return arr[n - 1];
     }
-
 }
 

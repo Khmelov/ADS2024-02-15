a9b0e9bb33252de46febe8c5bd996ea994152ad0
bnvch-polina <153857837+bnvch-polina@users.noreply.github.com>
2024-02-29 01:13:47 +0300

    Update FiboC.java

--- src/by/it/group310902/sverchkov/lesson01/FiboC.java
+++ src/by/it/group310902/sverchkov/lesson01/FiboC.java
@@ -34,7 +34,7 @@ public class FiboC {
         long prev = 0;
         int k = 1;
         ArrayList<Long> fiboL = new ArrayList<>();
-        fiboL.add(Long.valueOf("0"));
+        fiboL.addFirst(Long.valueOf("0"));
         fiboL.add(1,Long.valueOf("1"));
         for (int i = 1; i < n*n; i++) {
             long temp = cur;

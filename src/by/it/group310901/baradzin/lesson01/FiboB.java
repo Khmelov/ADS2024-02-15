4e55f35c87e3f3aad999465f18081e1c11a64a1b
arseniy <arsenjik12@gmail.com>
2024-02-29 00:40:46 +0300

    Lesson2

--- src/by/it/group310901/baradzin/lesson01/FiboB.java
+++ src/by/it/group310901/baradzin/lesson01/FiboB.java
@@ -12,7 +12,7 @@ public class FiboB {
 
     // Вычисление чисел простым быстрым методом
     public static void main(String[] args) {
-        var fibo = new FiboB();
+        FiboB fibo = new FiboB();
         int n = 55555;
         System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
     }
@@ -23,11 +23,14 @@ public class FiboB {
 
     // Здесь нужно реализовать вариант с временем O(n) и памятью O(n)
     BigInteger fastB(Integer n) {
-        var arr = new BigInteger[n + 1];
-        arr[0] = BigInteger.ZERO;
-        arr[1] = BigInteger.ONE;
-        for (int i = 2; i <= n; i++)
-            arr[i] = arr[i - 2].add(arr[i - 1]);
-        return arr[n];
+        BigInteger[] allNums = new BigInteger[n];
+        if (n == 1) return BigInteger.ONE;
+        if (n == 0) return BigInteger.ZERO;
+        allNums[0]=BigInteger.ZERO;
+        allNums[1]=BigInteger.ONE;
+        for(int i = 2; i<allNums.length; i++){
+            allNums[i] = allNums[i-1].add(allNums[i-2]);
+        }
+        return allNums[n-1].add(allNums[n-2]);
     }
 }

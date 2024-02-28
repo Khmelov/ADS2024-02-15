4647cca020020318671cf5a0fdbc43fbeb92c66a
HONOR <kuchkomaxim2527@gmail.com>
2024-02-29 00:02:23 +0300

    Lesson2 ABC

--- src/by/it/group351004/kuchko/lesson02/B_Sheduler.java
+++ src/by/it/group351004/kuchko/lesson02/B_Sheduler.java
@@ -1,7 +1,6 @@
 package by.it.group351004.kuchko.lesson02;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 /*
 Даны интервальные события events
 реализуйте метод calcStartTimes, так, чтобы число принятых к выполнению
@@ -28,7 +27,8 @@ public class B_Sheduler {
 
     public static void main(String[] args) {
         B_Sheduler instance = new B_Sheduler();
-        Event[] events = {  new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
+        Event[] events = {
+                new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
                 new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
                 new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
                 new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
@@ -41,19 +41,18 @@ public class B_Sheduler {
     }
 
     List<Event> calcStartTimes(Event[] events, int from, int to) {
-        //Events - события которые нужно распределить в аудитории
-        //в период [from, int] (включительно).
         //оптимизация проводится по наибольшему числу непересекающихся событий.
         //Начало и конец событий могут совпадать.
         List<Event> result;
         result = new ArrayList<>();
         //ваше решение.
-
-
-
-
-
-
+        Arrays.sort(events, Comparator.comparingInt(event -> event.stop));
+        result.add(events[0]);
+        for (Event event : events) {
+            if (result.get(result.size() - 1).stop <= event.start) {
+                result.add(event);
+            }
+        }
         return result;          //вернем итог
     }
 }
--- src/by/it/group351004/kuchko/lesson02/C_GreedyKnapsack.java
+++ src/by/it/group351004/kuchko/lesson02/C_GreedyKnapsack.java
@@ -25,7 +25,6 @@ public class C_GreedyKnapsack {
             this.cost = cost;
             this.weight = weight;
         }
-
         @Override
         public String toString() {
             return "Item{" +
@@ -38,11 +37,36 @@ public class C_GreedyKnapsack {
         public int compareTo(Item o) {
             //тут может быть ваш компаратор
 
-
             return 0;
         }
     }
-
+    private static int part(Item[] arr, int low, int high) {
+        int middle = low + (high - low) / 2;
+        Item pivot = arr[middle];
+        Item temp = arr[middle];
+        arr[middle] = arr[high];
+        arr[high] = temp;
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j].cost / arr[j].weight > pivot.cost / pivot.weight) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+    static void quickSort(Item[] arr, int low, int high) {
+        if (low < high) {
+            int pi = part(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
     double calc(File source) throws FileNotFoundException {
         Scanner input = new Scanner(source);
         int n = input.nextInt();      //сколько предметов в файле
@@ -56,21 +80,20 @@ public class C_GreedyKnapsack {
             System.out.println(item);
         }
         System.out.printf("Всего предметов: %d. Рюкзак вмещает %d кг.\n",n,W);
-
-        //тут необходимо реализовать решение задачи
-        //итогом является максимально воможная стоимость вещей в рюкзаке
-        //вещи можно резать на кусочки (непрерывный рюкзак)
         double result = 0;
-        //тут реализуйте алгоритм сбора рюкзака
-        //будет особенно хорошо, если с собственной сортировкой
-        //кроме того, можете описать свой компаратор в классе Item
-
-        //ваше решение.
-
-
-
-
-
+        quickSort(items, 0, items.length - 1);
+        int i = 0;
+        while (W > 0) {
+            if (items[i].weight > W) {
+                result += W * items[i].cost / items[i].weight;
+                W = 0;
+            }
+            else {
+                result += items[i].cost;
+                W -= items[i].weight;
+                i++;
+            }
+        }
         System.out.printf("Удалось собрать рюкзак на сумму %f\n",result);
         return result;
     }

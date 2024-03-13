7cb7d9939814c39737af1388f5db441cddb04180
Tail <vitaliycof@outlook.com>
2024-03-13 21:06:57 +0300

    Сделал А из задания 3.

--- src/by/it/group351005/zhuravski/lesson03/A_Huffman.java
+++ src/by/it/group351005/zhuravski/lesson03/A_Huffman.java
@@ -150,7 +150,7 @@ public class A_Huffman {
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
         Node root = priorityQueue.poll();
-        root.fillCodes("");
+        root.fillCodes("");//
         StringBuilder sb = new StringBuilder();
         //.....
         for (int i = 0; i < s.length(); i++) {

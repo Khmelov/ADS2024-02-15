82a4c5c6bc995628915f2370a9ca3fa2046659e7
Tail <vitaliycof@outlook.com>
2024-03-13 22:36:52 +0300

    Сделал C.

--- src/by/it/group351005/zhuravski/lesson03/C_HeapMax.java
+++ src/by/it/group351005/zhuravski/lesson03/C_HeapMax.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Math.pow;
+
 // Lesson 3. C_Heap.
 // Задача: построить max-кучу = пирамиду = бинарное сбалансированное дерево на массиве.
 // ВАЖНО! НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ НИКАКИЕ КОЛЛЕКЦИИ, КРОМЕ ARRAYLIST (его можно, но только для массива)
@@ -41,24 +43,75 @@ public class C_HeapMax {
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
         //тут запишите ваше решение.
         //Будет мало? Ну тогда можете его собрать как Generic и/или использовать в варианте B
+        private List<List<Long>> layers = new ArrayList<>();
         private List<Long> heap = new ArrayList<>();
+
+        MaxHeap() {
+            layers.add(new ArrayList<>());
+        }
 
         int siftDown(int i) { //просеивание вверх
 
             return i;
         }
 
-        int siftUp(int i) { //просеивание вниз
-
-            return i;
+        void siftUp() { //просеивание вниз
+            int curLayer = layers.size() - 1;
+            int curLayerPos = layers.getLast().size() - 1;
+            int topLayerPos = curLayerPos / 2;
+            int topLayer = curLayer - 1;
+            long temp;
+            while (topLayer >= 0) {
+                if (curLayerPos % 2 == 0) {
+                    if (layers.get(topLayer).get(topLayerPos) > layers.get(curLayer).get(curLayerPos)) {
+                        break;
+                    }
+                    else {
+                        temp = layers.get(topLayer).get(topLayerPos);
+                        layers.get(topLayer).set(topLayerPos, layers.get(curLayer).get(curLayerPos));
+                        layers.get(curLayer).set(curLayerPos, temp);
+                        curLayer--;
+                        topLayer--;
+                        curLayerPos = topLayerPos;
+                        topLayerPos = curLayerPos / 2;
+                    }
+                }
+                else {
+                    if (layers.get(topLayer).get(topLayerPos) < layers.get(curLayer).get(curLayerPos)) {
+                        break;
+                    }
+                    else {
+                        temp = layers.get(topLayer).get(topLayerPos);
+                        layers.get(topLayer).set(topLayerPos, layers.get(curLayer).get(curLayerPos));
+                        layers.get(curLayer).set(curLayerPos, temp);
+                        curLayer--;
+                        topLayer--;
+                        curLayerPos = topLayerPos;
+                        topLayerPos = curLayerPos / 2;
+                    }
+                }
+            }
         }
 
         void insert(Long value) { //вставка
+            int curMax = (int) pow(2,(layers.size() - 1));
+            if (layers.getLast().size() == curMax) {
+                layers.add(new ArrayList<>());
+            }
+            layers.getLast().add(value);
+            siftUp();;
         }
 
         Long extractMax() { //извлечение и удаление максимума
-            Long result = null;
-
+            Long result = layers.getLast().get(0);
+            int pos = 0;//Не помешшала бы проверка на пустое дерево
+            for (int i = 1; i < layers.getLast().size(); i++) {
+                if (layers.getLast().get(i) > result) {//Но было принято решение забить
+                    result = layers.getLast().get(i);//Всё равно это быдлокод
+                    pos = i;
+                }
+            }
+            layers.getLast().remove(pos);
             return result;
         }
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! КОНЕЦ ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1

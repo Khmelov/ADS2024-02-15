a5ecac25677a9ca54b99abf11b9076810ba88295
pavello06 <pavelgaluha@gmail.com>
2024-03-14 22:55:17 +0300

    Lesson 3 ABC

--- src/by/it/group351005/pavello06/lesson03/A_Huffman.java
+++ src/by/it/group351005/pavello06/lesson03/A_Huffman.java
@@ -141,22 +141,14 @@ public class A_Huffman {
         }
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
-<<<<<<< HEAD
         Node root = priorityQueue.poll();
         root.fillCodes("");
-=======
->>>>>>> origin/main
+
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
             sb.append(codes.get(c));
         }
         //.....
-<<<<<<< HEAD
-
-=======
-        Node root = priorityQueue.poll();
-        root.fillCodes("");
->>>>>>> origin/main
 
         return sb.toString();
         //01001100100111

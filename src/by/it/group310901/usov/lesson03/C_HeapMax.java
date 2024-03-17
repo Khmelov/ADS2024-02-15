743c61cd04abdb0795063debae5dad2283901651
Usov <usov.lesha2017@gmail.com>
2024-03-17 19:50:24 +0300

    solve the problems С

--- src/by/it/group310901/usov/lesson03/C_HeapMax.java
+++ src/by/it/group310901/usov/lesson03/C_HeapMax.java
@@ -43,22 +43,43 @@ public class C_HeapMax {
         //Будет мало? Ну тогда можете его собрать как Generic и/или использовать в варианте B
         private List<Long> heap = new ArrayList<>();
 
-        int siftDown(int i) { //просеивание вверх
-
+        int siftUp(int i) { //просеивание вверх
+            while (heap.get(i) > heap.get((i - 1) / 2)){
+                swap(i, (i - 1) / 2);
+                i = (i - 1) / 2;
+            }
             return i;
         }
-
-        int siftUp(int i) { //просеивание вниз
-
+        int siftDown(int i) { //просеивание вниз
+            while (2*i + 1 < heap.size()) {
+                int left = 2 * i + 1;
+                int right = 2 * i + 2;
+                int max = left;
+                if((right < heap.size()) && (heap.get(right) > heap.get(left)))
+                    max = right;
+                if(i == max)
+                    break;
+                i = max;
+            }
             return i;
         }
 
         void insert(Long value) { //вставка
+            heap.add(value);
+            siftUp(heap.size()-1);
+        }
+
+        private void swap(int i, int j){
+            Long temp = heap.get(j);
+            heap.set(j, heap.get(i));
+            heap.set(i, temp);
         }
 
         Long extractMax() { //извлечение и удаление максимума
             Long result = null;
-
+            result = heap.get(0);
+            heap.remove(0);
+            siftDown(0);
             return result;
         }
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! КОНЕЦ ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1

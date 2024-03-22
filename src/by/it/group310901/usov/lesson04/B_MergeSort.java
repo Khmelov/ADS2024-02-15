7fb29711b093fd825dde1e0877d50d04795d64a2
Usov <usov.lesha2017@gmail.com>
2024-03-23 00:00:52 +0300

    solve the problem A and B

---
+++ src/by/it/group310901/usov/lesson04/B_MergeSort.java
@@ -0,0 +1,82 @@
+package by.it.group310901.usov.lesson04;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class B_MergeSort {
+
+    void merge(int[] a, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            leftArray[i] = a[left + i];
+        for (int j = 0; j < n2; j++)
+            rightArray[j] = a[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                a[k] = leftArray[i];
+                i++;
+            } else {
+                a[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            a[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            a[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(int[] a, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(a, left, mid);
+            mergeSort(a, mid + 1, right);
+
+            merge(a, left, mid, right);
+        }
+    }
+
+    int[] getMergeSort(InputStream stream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(stream);
+
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        mergeSort(a, 0, n - 1);
+
+        return a;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataB.txt");
+        B_MergeSort instance = new B_MergeSort();
+        int[] result = instance.getMergeSort(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+}

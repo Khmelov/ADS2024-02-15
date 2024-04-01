ee4959b554ac274aaf2f73e907856be082d88513
Nadeŭka <nadevko@riseup.net>
2024-04-01 20:21:01 +0300

    revert java 21 preview to 21 lts

--- src/by/it/group310901/baradzin/lesson03/Lesson03Test.java
+++ src/by/it/group310901/baradzin/lesson03/Lesson03Test.java
@@ -11,8 +11,8 @@ import static org.junit.Assert.assertTrue;
 public class Lesson03Test {
     @Test
     public void A() throws Exception {
-        var root = STR."\{System.getProperty("user.dir")}/src/";
-        var f = new File(STR."\{root}by/it/group310901/baradzin/lesson03/dataHuffman.txt");
+        var root = System.getProperty("user.dir") + "/src/";
+        var f = new File(root + "by/it/group310901/baradzin/lesson03/dataHuffman.txt");
         var instance = new A_Huffman();
         var result = instance.encode(f);
         var ok = result.equals("01001100100111");
@@ -21,8 +21,8 @@ public class Lesson03Test {
 
     @Test
     public void B() throws Exception {
-        var root = STR."\{System.getProperty("user.dir")}/src/";
-        var f = new File(STR."\{root}by/it/group310901/baradzin/lesson03/encodeHuffman.txt");
+        var root = System.getProperty("user.dir") + "/src/";
+        var f = new File(root + "by/it/group310901/baradzin/lesson03/encodeHuffman.txt");
         var instance = new B_Huffman();
         var result = instance.decode(f);
         var ok = result.equals("abacabad");
@@ -31,8 +31,8 @@ public class Lesson03Test {
 
     @Test
     public void C() throws Exception {
-        var root = STR."\{System.getProperty("user.dir")}/src/";
-        InputStream stream = new FileInputStream(STR."\{root}by/it/group310901/baradzin/lesson03/heapData.txt");
+        var root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group310901/baradzin/lesson03/heapData.txt");
         var instance = new C_HeapMax();
         var res = instance.findMaxValue(stream);
         var ok = (res == 500);

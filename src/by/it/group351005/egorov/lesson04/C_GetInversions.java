2ca8024c19fed49ecf36c7e3d41371e904f660b5
Aleeg0 <Sankafoxe@gmail.com>
2024-04-02 16:46:30 +0300

    Solve 3 task.

--- src/by/it/group351005/egorov/lesson04/C_GetInversions.java
+++ src/by/it/group351005/egorov/lesson04/C_GetInversions.java
@@ -35,6 +35,51 @@ Sample Output:
 
 public class C_GetInversions {
 
+    int inversions;
+    void merger(int[] a,int l,int m,int r){
+        // make a copies of arrays
+        int length1 = m - l;
+        int length2 = r - m + 1;
+        int[] leftArray = new int[length1];
+        for (int i = 0; i < length1; i++) {
+            leftArray[i] = a[l+i];
+        }
+        int[] rightArray = new int[length2];
+        for (int i = 0; i < length2; i++) {
+            rightArray[i] = a[m+i];
+        }
+        //merging
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while(k <= r){
+            if (i == length1){
+                a[k++] = rightArray[j++];
+            }
+            else if (j == length2){
+                a[k++] = leftArray[i++];
+            }
+            else if (leftArray[i] <= rightArray[j]){
+                a[k++] = leftArray[i++];
+            }
+            else{
+                a[k++] = rightArray[j++];
+                // счетчик
+                inversions += length1 - i;
+            }
+        }
+    }
+
+    void mergeSorter (int[] a, int l,int r){
+        if (r <= l)
+            return;
+        int m = (r + l) /2;
+        mergeSorter(a,l,m);
+        mergeSorter(a,m+1,r);
+        merger(a,l,m+1,r);
+
+    }
+
     int calc(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
@@ -46,19 +91,11 @@ public class C_GetInversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int result = 0;
         //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
-
-
-
-
+        inversions = 0;
+        mergeSorter(a,0,n-1);
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        return inversions;
     }
 
 

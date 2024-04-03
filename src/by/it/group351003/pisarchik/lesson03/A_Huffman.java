55371768f6f267c1a56590a7356c84352d592f03
elisabeth <pliza210306@gmail.com>
2024-04-03 20:42:28 +0300

    Lesson 4: A, B, C

--- src/by/it/group351003/pisarchik/lesson03/A_Huffman.java
+++ src/by/it/group351003/pisarchik/lesson03/A_Huffman.java
@@ -121,7 +121,7 @@ public class A_Huffman {
 
         Map<Character, Integer> count = new HashMap<>();
         //1. переберем все символы по очереди и рассчитаем их частоту в Map count
-            //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
+        //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
         for (int i = 0; i < s.length(); i++) {
             if (!count.containsKey(s.charAt(i)))
                 count.put(s.charAt(i), 1);
@@ -131,11 +131,10 @@ public class A_Huffman {
 
         //2. перенесем все символы в приоритетную очередь в виде листьев
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-        for (Character ch: count.keySet()) {
+        for (Character ch : count.keySet()) {
             Node node = new LeafNode(count.get(ch), ch);
             priorityQueue.add(node);
         }
-
         //3. вынимая по два узла из очереди (для сборки родителя)
         //и возвращая этого родителя обратно в очередь
         //построим дерево кодирования Хаффмана.
@@ -147,10 +146,10 @@ public class A_Huffman {
             root = new InternalNode(node1, node2);
             priorityQueue.add(root);
         }
-
         //4. последний из родителей будет корнем этого дерева
-        //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
+        //это будет последний и единственный элемент оставшийся в очереди priorityQueue
         StringBuilder sb = new StringBuilder();
+        root.fillCodes("");
         for (int i = 0; i < s.length(); i++)
             sb.append(codes.get(s.charAt(i)));
         return sb.toString();

293cb5bb7f7d74a1e65c79d78107b162bc121a17
Nektto <perova.violetta10work@gmail.com>
2024-04-04 02:49:08 +0300

    Lesson06 A,B,C

--- src/by/it/group310902/perova/lesson06/B_LongDivComSubSeq.java
+++ src/by/it/group310902/perova/lesson06/B_LongDivComSubSeq.java
@@ -34,7 +34,7 @@ public class B_LongDivComSubSeq {
     int getDivSeqSize(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //!!!!!!!!!!!!!!!!!!!!!!!!!    НАЧАЛО ЗАДАЧИ    !!!!!!!!!!!!!!!!!!!!!!!!!
         //общая длина последовательности
         int n = scanner.nextInt();
         int[] m = new int[n];

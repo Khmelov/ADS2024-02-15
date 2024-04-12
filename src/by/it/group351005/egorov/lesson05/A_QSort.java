9a42467e5b6b8020d179237c03ac15c47601e193
Aleeg0 <Sankafoxe@gmail.com>
2024-04-12 16:50:41 +0300

    solve task A

--- src/by/it/group351005/egorov/lesson05/A_QSort.java
+++ src/by/it/group351005/egorov/lesson05/A_QSort.java
@@ -52,10 +52,50 @@ public class A_QSort {
         @Override
         public int compareTo(Segment o) {
             //подумайте, что должен возвращать компаратор отрезков
-
-            return 0;
+            if (this.start > o.start) {
+                return 1;
+            }
+            else if (this.start == o.start){
+                if (this.stop > o.stop) {
+                    return 1;
+                }
+                else if (this.stop == o.stop){
+                    return 0;
+                }
+                return -1;
+            }
+            return -1;
         }
     }
+
+    public int Partition(Segment[] arr, int left,int right){
+        Segment pivot = arr[(left + right) / 2];
+        while (left <= right){
+            while (arr[left].compareTo(pivot) == -1){
+                left++;
+            }
+            while (arr[right].compareTo(pivot) == 1){
+                right--;
+            }
+            if (left <= right){
+                Segment temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    public void QuickSort(Segment[] arr, int left, int right){
+        if (right <= left)
+            return;
+        int partition_index = Partition(arr, left, right);
+        QuickSort(arr,left,partition_index - 1);
+        QuickSort(arr,partition_index,right);
+    }
+
 
 
     int[] getAccessory(InputStream stream) throws FileNotFoundException {
@@ -73,7 +113,7 @@ public class A_QSort {
         //читаем сами отрезки
         for (int i = 0; i < n; i++) {
             //читаем начало и конец каждого отрезка
-            segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
+            segments[i] = new Segment(scanner.nextInt(),scanner.nextInt());
         }
         //читаем точки
         for (int i = 0; i < m; i++) {
@@ -81,8 +121,19 @@ public class A_QSort {
         }
         //тут реализуйте логику задачи с применением быстрой сортировки
         //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
-
-
+        QuickSort(segments,0, segments.length - 1);
+        int start = 0;
+        int end = 0;
+        for (Segment segment : segments){
+             start = segment.start;
+             end = segment.stop;
+             for (int i = 0; i < points.length; i++){
+                 if (end < points[i])
+                     break;
+                 if (start <= points[i])
+                     result[i]++;
+             }
+        }
 
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!

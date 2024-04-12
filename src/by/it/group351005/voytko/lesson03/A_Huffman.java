c6d00f3391971fa4a922ce1062c598c04367060e
Wholito <voitkotm@gmail.com>
2024-04-12 21:56:58 +0300

    Lesson3

--- src/by/it/group351005/voytko/lesson03/A_Huffman.java
+++ src/by/it/group351005/voytko/lesson03/A_Huffman.java
@@ -122,18 +122,32 @@ public class A_Huffman {
         Map<Character, Integer> count = new HashMap<>();
         //1. переберем все символы по очереди и рассчитаем их частоту в Map count
             //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
-
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
         //2. перенесем все символы в приоритетную очередь в виде листьев
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-
+        for (Map.Entry<Character, Integer> entry : count.entrySet()) {
+            priorityQueue.add(new LeafNode(entry.getValue(), entry.getKey()));
+        }
         //3. вынимая по два узла из очереди (для сборки родителя)
         //и возвращая этого родителя обратно в очередь
         //построим дерево кодирования Хаффмана.
         //У родителя частоты детей складываются.
-
+        while (priorityQueue.size() > 1) {
+            Node left = priorityQueue.poll();
+            Node right = priorityQueue.poll();
+            priorityQueue.add(new InternalNode(left, right));
+        }
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
+        Node root = priorityQueue.poll();
+        root.fillCodes("");
+
         StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            sb.append(codes.get(c));
+        }
         //.....
 
         return sb.toString();

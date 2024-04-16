5bc244ef87c3c24901833b39b1a69906c57abaf8
Aleeg0 <Sankafoxe@gmail.com>
2024-04-16 20:20:30 +0300

    Solve Task B

--- src/by/it/group351005/egorov/lesson06/B_LongDivComSubSeq.java
+++ src/by/it/group351005/egorov/lesson06/B_LongDivComSubSeq.java
@@ -43,9 +43,19 @@ public class B_LongDivComSubSeq {
             m[i] = scanner.nextInt();
         }
         //тут реализуйте логику задачи методами динамического программирования (!!!)
+        int[] temp = new int[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (m[i] % m[j] == 0 && temp[i] < temp[j] + 1)
+                    temp[i] = temp[j] + 1;
+            }
+        }
         int result = 0;
-
-
+        for (int i = 0; i < n; i++) {
+            if (result < temp[i])
+                result = temp[i];
+        }
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }

f7c118cdb79d181225634d5e5a3849385d150bcd
krisssstt <kristtinakovalyova23@gmail.com>
2024-04-25 12:05:08 +0300

    Lesson5 A,B,C

--- src/by/it/group310901/kovalyova/lesson05/C_QSortOptimized.java
+++ src/by/it/group310901/kovalyova/lesson05/C_QSortOptimized.java
@@ -89,7 +89,6 @@ public class C_QSortOptimized {
         var temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
-
     }
     int find(Segment[] arr, int target) {
         return find(arr, target, 0, arr.length - 1);

bce198cad9ab5dd6726d770d9fdf4d6f33dd139d
HONOR <kuchkomaxim2527@gmail.com>
2024-05-02 23:31:04 +0300

    Lesson 5 ABC

--- src/by/it/group351004/kuchko/lesson05/B_CountSort.java
+++ src/by/it/group351004/kuchko/lesson05/B_CountSort.java
@@ -43,7 +43,7 @@ public class B_CountSort {
         for (int val: points)
             tempArr[val]++;
         int b = 0;
-        for (int i = 0; i < 10; ++i){
+        for (int i = 0; i < 10; ++i) {
             for (int j = 0; j < tempArr[i]; ++j) {
                 points[b++] = i;
             }

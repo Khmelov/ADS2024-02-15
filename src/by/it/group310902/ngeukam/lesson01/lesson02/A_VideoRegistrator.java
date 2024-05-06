5bf0a2100bfe72f213a8008ad44b532f6286dc62
Noriskcontent <deutoupatrick83@gmail.com>
2024-05-06 17:26:28 +0300

    lesson 1,2,3

--- src/by/it/group310902/ngeukam/lesson01/lesson02/A_VideoRegistrator.java
+++ src/by/it/group310902/ngeukam/lesson01/lesson02/A_VideoRegistrator.java
@@ -1,4 +1,4 @@
-package by.it.group310902.yershov.lesson01.lesson02;
+package by.it.group310902.ngeukam.lesson01.lesson02;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +13,14 @@ import java.util.List;
 public class A_VideoRegistrator {
 
     public static void main(String[] args) {
-        A_VideoRegistrator instance=new A_VideoRegistrator();
-        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        A_VideoRegistrator instance = new A_VideoRegistrator();
+        double[] events = new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts = instance.calcStartTimes(events, 1); //рассчитаем моменты старта, с длинной сеанса 1
         System.out.println(starts);                            //покажем моменты старта
     }
+
     //модификаторы доступа опущены для возможности тестирования
-    List<Double> calcStartTimes(double[] events, double workDuration){
+    List<Double> calcStartTimes(double[] events, double workDuration) {
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;

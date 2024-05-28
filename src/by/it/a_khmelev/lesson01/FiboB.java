17fb4ff133879a00e80bd5af74e18c6f73caf46d
Veronika Kashlej <veronika.kashlei@mail.ru>
2024-05-28 23:39:35 +0300

    no changes with files by hmelev

--- src/by/it/a_khmelev/lesson01/FiboB.java
+++ src/by/it/a_khmelev/lesson01/FiboB.java
@@ -31,8 +31,8 @@ public class FiboB {
         FiboArr[1] = BigInteger.ONE;
         for (int i = 2; i < n; i++)
             FiboArr[i] = FiboArr[i - 1].add(FiboArr[i - 2]);
-
-
+        return FiboArr[n - 1];
+=======
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
         return BigInteger.valueOf(-1L);
       

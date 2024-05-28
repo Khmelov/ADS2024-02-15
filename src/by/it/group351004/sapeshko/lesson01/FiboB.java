189dbbc72dd0f2b031e42bed4513dfdd04202221
Sapeshko <sapeshko2000@mail.ru>
2024-05-28 22:30:33 +0300

    Lessons 7 and 8

--- src/by/it/group351004/sapeshko/lesson01/FiboB.java
+++ src/by/it/group351004/sapeshko/lesson01/FiboB.java
@@ -12,30 +12,28 @@ public class FiboB {
 
     private long startTime = System.currentTimeMillis();
 
-    public static void main(String[] args) {
-        //вычисление чисел простым быстрым методом
-        FiboB fibo = new FiboB();
-        int n = 55555;
-        System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
-    }
-
     private long time() {
         return System.currentTimeMillis() - startTime;
     }
 
+    public static void main(String[] args) {
+
+        //вычисление чисел простым быстрым методом
+        by.it.group351004.sapeshko.lesson01.FiboB fibo = new by.it.group351004.sapeshko.lesson01.FiboB();
+        int n = 55555;
+        System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
+    }
+
     BigInteger fastB(Integer n) {
+        BigInteger[] fibo = new BigInteger [n+1];
+        fibo[0] = BigInteger.ZERO;
+        fibo[1] = BigInteger.ONE;
 
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i-1].add(fibo[i-2]);
+        }
 
-        BigInteger[] FiboArr = new BigInteger[n];
-        FiboArr[0] = BigInteger.ONE;
-        FiboArr[1] = BigInteger.ONE;
-        for (int i = 2; i < n; i++)
-            FiboArr[i] = FiboArr[i - 1].add(FiboArr[i - 2]);
-        return FiboArr[n - 1];
-=======
-        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        return BigInteger.valueOf(-1L);
-      
+        return fibo[n];
     }
 
 }

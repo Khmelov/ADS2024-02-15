3334f2f7f7b5559e452985f7c97f01b56cafe686
am am <sadertyolp@gmail.com>
2024-05-29 12:12:23 +0300

    lessons 5,6,7,8

--- src/by/it/group310901/lisovitskii/lesson03/Lesson03Test.java
+++ src/by/it/group310901/lisovitskii/lesson03/Lesson03Test.java
@@ -15,14 +15,14 @@ public class Lesson03Test {
     Name:               Test a_khmelev (тут ваша фамилия)
     Test kind:          All in package
     Package:            by.it.a_khmelev (тут ваша фамилия)
-    Search for test:    In whole project
+    Sear>ch for test:    In whole project
     */
 
 
     @Test
     public void A() throws Exception {
         String root = System.getProperty("user.dir") + "/src/";
-        File f = new File(root + "by/it/a_khmelev/lesson03/dataHuffman.txt");
+        File f = new File(root + "by/it/a_khmelev/lesson03/dataA.txt");
         A_Huffman instance = new A_Huffman();
         String result = instance.encode(f);
         boolean ok=result.equals("01001100100111");
@@ -32,7 +32,7 @@ public class Lesson03Test {
     @Test
     public void B() throws Exception {
         String root = System.getProperty("user.dir") + "/src/";
-        File f = new File(root + "by/it/a_khmelev/lesson03/encodeHuffman.txt");
+        File f = new File(root + "by/it/a_khmelev/lesson03/dataB.txt");
         B_Huffman instance = new B_Huffman();
         String result = instance.decode(f);
         boolean ok=result.equals("abacabad");
@@ -41,7 +41,7 @@ public class Lesson03Test {
     @Test
     public void C() throws Exception {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson03/heapData.txt");
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson03/dataC.txt");
         C_HeapMax instance = new C_HeapMax();
         Long res=instance.findMaxValue(stream);
         boolean ok=(res==500);

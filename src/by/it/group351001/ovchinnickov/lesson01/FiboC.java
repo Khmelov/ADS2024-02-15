a12929d477335f0496aaaf1b06bdd47715b4800d
ovsm1 <143704194+ovsm1@users.noreply.github.com>
2024-05-29 11:27:13 +0300

    lesson1 lesson2 lesson3 lesson4 lesson5 lesson6 lesson7 lesson8

--- src/by/it/group351001/ovchinnickov/lesson01/FiboC.java
+++ src/by/it/group351001/ovchinnickov/lesson01/FiboC.java
@@ -6,8 +6,6 @@ package by.it.group351001.ovchinnickov.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
-import java.math.BigInteger;
-
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -23,51 +21,42 @@ public class FiboC {
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
-    long fasterC(long n, int m) {
-        BigInteger M = BigInteger.valueOf(m);
-        BigInteger a = BigInteger.ZERO;
-        BigInteger b = BigInteger.ONE;
-        BigInteger t;
+    int searchperiod(int m) {
+        int period= 0;
+        int prev=0;
+        int curr=1;
+        int next;
 
-        long count = 2;
-        for (int i = 1; i <= m * m -  1; i++) {
-            t = a.add(b);
 
-            if (t.mod(M).equals(BigInteger.ONE) && i!= 1 )
-                if (b.mod(M).equals(BigInteger.ONE)  )
-                    if (a.mod(M).equals(BigInteger.ZERO) )
-                    {
-                        count = i;
-                        break;
-                    }
-
-            a = b;
-            b = t;
+        for(int i=0; i<m*6; i++) {
+            next = (prev+curr) % m;
+            prev=curr;
+            curr=next;
+            if (prev==0 && curr==1)
+                period=i+1;
 
         }
-        count = n % (count) ;
-
-        long otevet = 1;
-
-        a = BigInteger.ZERO;
-        b = BigInteger.ONE;
-        t = BigInteger.ZERO;
-
-        if (n <= 2)
-        {
-            otevet = 1;
-        }
-        else
-            for (int i = 1; i <= count; i++) {
-                otevet = t.mod(M).intValue();
-                t = a.add(b);
-                a = b;
-                b = t;
-            }
-
-        //Решение сложно найти интуитивно
-        //возможно потребуется дополнительный поиск информации
-        //см. период Пизано
-        return otevet;
+        return period;
     }
+    long fasterC(long n, int m)
+    {
+
+        int period=searchperiod(m);
+        long res= n % period;
+        int prev=0;
+        int curr=1;
+        int next=0;
+        if (n==0) return 0;
+        if (n==1) return 1;
+        else
+            for (int i=0; i<res-1; i++ ) {
+                next = (prev+curr) % m;
+                prev=curr;
+                curr=next;
+            }
+        return curr % m;
+    }
+
+
 }
+

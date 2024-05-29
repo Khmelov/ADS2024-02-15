8a2ddfb4bbe8992bc95e0103ac11759801888f5d
MaryniaLis <jokerjjj0638@gmail.com>
2024-05-29 19:17:47 +0300

    Lesson1 A, B, C; Lesson2 A, B, C; Lesson3 A, B, C; Lesson4 A, B, C; Lesson5 A, B, C; Lesson6 A, B, C; Lesson7 A, B, C; Lesson8 A, B, C.

--- src/by/it/group351003/lisunova/lesson01/FiboB.java
+++ src/by/it/group351003/lisunova/lesson01/FiboB.java
@@ -1,10 +1,9 @@
-package by.it.a_khmelev.lesson01;
+package by.it.group351003.lisunova.lesson01;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 
 /*
- * Вам необходимо выполнить способ вычисления чисел Фибоначчи со вспомогательным массивом
+ * Вам необходимо выполнить способ вычисления чисел Фибоначчи с вспомогательным массивом
  * без ограничений на размер результата (BigInteger)
  */
 
@@ -12,30 +11,31 @@ public class FiboB {
 
     private long startTime = System.currentTimeMillis();
 
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
     public static void main(String[] args) {
+
         //вычисление чисел простым быстрым методом
         FiboB fibo = new FiboB();
         int n = 55555;
         System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
     }
 
-    private long time() {
-        return System.currentTimeMillis() - startTime;
-    }
-
     BigInteger fastB(Integer n) {
-
-
-        BigInteger[] FiboArr = new BigInteger[n];
-        FiboArr[0] = BigInteger.ONE;
-        FiboArr[1] = BigInteger.ONE;
-        for (int i = 2; i < n; i++)
-            FiboArr[i] = FiboArr[i - 1].add(FiboArr[i - 2]);
-        return FiboArr[n - 1];
-
         //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-        return BigInteger.valueOf(-1L);
-      
+        if (n == 0)
+            return BigInteger.ZERO;
+        if (n == 1)
+            return BigInteger.ONE;
+        n++;
+        BigInteger arr[] = new BigInteger[n];
+        arr[0] = BigInteger.ZERO;
+        arr[1] = BigInteger.ONE;
+        for (int i = 2; i < n; i++)
+            arr[i] = arr[i - 1].add(arr[i - 2]);
+        return arr[n - 1];
     }
 
 }

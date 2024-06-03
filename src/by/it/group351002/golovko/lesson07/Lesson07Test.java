4fb0e8595b370708f8af2272770d06e73209539e
Foliish <halaukokostya@gmail.com>
2024-06-03 10:22:11 +0300

    les1

--- src/by/it/group351002/golovko/lesson07/Lesson07Test.java
+++ src/by/it/group351002/golovko/lesson07/Lesson07Test.java
@@ -1,4 +1,4 @@
-package by.it.a_khmelev.lesson07;
+package by.it.group351002.golovko.lesson07;
 
 import org.junit.Test;
 
--- src/by/it/group351002/golovko/lesson08/Lesson08Test.java
+++ src/by/it/group351002/golovko/lesson08/Lesson08Test.java
@@ -1,4 +1,4 @@
-package by.it.a_khmelev.lesson08;
+package by.it.group351002.golovko.lesson08;
 
 import org.junit.Test;
 

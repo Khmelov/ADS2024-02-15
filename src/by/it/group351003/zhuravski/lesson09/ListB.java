3045d5dea093666cadcac2e30c3291cf7c316920
Tail <vitaliycof@outlook.com>
2024-09-06 23:36:49 +0300

    Сделал B из lesson 9.

--- src/by/it/group351003/zhuravski/lesson09/ListB.java
+++ src/by/it/group351003/zhuravski/lesson09/ListB.java
@@ -3,7 +3,17 @@ package by.it.group351003.zhuravski.lesson09;
 import java.util.*;
 
 public class ListB<E> implements List<E> {
-
+    public class ListElem {
+        E value;
+        ListElem next;
+        public ListElem(E value, ListElem next) {
+            this.next = next;
+            this.value = value;
+        }
+    };
+    private ListElem first = null;
+    private ListElem last = null;
+    private int count = 0;
 
     //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
 
@@ -14,36 +24,97 @@ public class ListB<E> implements List<E> {
     /////////////////////////////////////////////////////////////////////////
     @Override
     public String toString() {
-        return "";
+        String result = "[";
+        if (count > 0) {
+            ListElem cur = first;
+            for (int i = 1; i < count; i++) {
+                result = result.concat(cur.value.toString());
+                result = result.concat(", ");
+                cur = cur.next;
+            }
+            result = result.concat(cur.value.toString());
+        }
+        result += "]";
+        return result;
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        if (last == null) {
+            first = new ListElem(e, null);
+            last = first;
+        }
+        else {
+            ListElem v = new ListElem(e, null);
+            last.next = v;
+            last = v;
+        }
+        count += 1;
+        return true;
     }
 
     @Override
     public E remove(int index) {
+        if (index < count) {
+            ListElem cur = first;
+            for (int i = 2; i <= index; i++) {
+                cur = cur.next;
+            }
+            E res = cur.next.value;
+            cur.next = cur.next.next;
+            count -= 1;
+            return res;
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     @Override
     public void add(int index, E element) {
-
+        if (index == 0) {
+            ListElem n = new ListElem(element, first);
+            first = n;
+            count++;
+        }
+        else if (index == count - 1) {
+            add(element);
+        }
+        else {
+            ListElem cur = first;
+            for (int i = 2; i <= index; i++) {
+                cur = cur.next;
+            }
+            ListElem n = new ListElem(element, cur.next);
+            cur.next = n;
+            count++;
+        }
     }
 
     @Override
     public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index != -1) {
+            remove(index);
+            return true;
+        }
         return false;
     }
 
     @Override
     public E set(int index, E element) {
+        if (index < count) {
+            ListElem cur = first;
+            for (int i = 1; i <= index; i++) {
+                cur = cur.next;
+            }
+            E old = cur.value;
+            cur.value = element;
+            return old;
+        }
         return null;
     }
 
@@ -56,27 +127,65 @@ public class ListB<E> implements List<E> {
 
     @Override
     public void clear() {
-
+        count = 0;
+        first = null;
+        last = null;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        if (count > 0) {
+            int index = 0;
+            ListElem cur = first;
+            while (index < count) {
+                if (cur.value.equals(o)) {
+                    return index;
+                }
+                index++;
+                cur = cur.next;
+            }
+            return -1;
+        }
+        return -1;
     }
 
     @Override
     public E get(int index) {
+        if (index < count) {
+            ListElem cur = first;
+            for (int i = 1; i <= index; i++) {
+                cur = cur.next;
+            }
+            return cur.value;
+        }
         return null;
     }
 
     @Override
     public boolean contains(Object o) {
+        int index = indexOf(o);
+        if (index > -1) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        if (count > 0) {
+            int index = 0;
+            int res = -1;
+            ListElem cur = first;
+            while (index < count) {
+                if (cur.value.equals(o)) {
+                    res = index;;
+                }
+                index++;
+                cur = cur.next;
+            }
+            return res;
+        }
+        return -1;
     }
 
 

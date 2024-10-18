6eeb90008aef73f6206e7c3597811edcff223510
Aleeg0 <Sankafoxe@gmail.com>
2024-10-18 21:34:07 +0300

    feat(lesson12): solve taskB Add methods: headMap, tailMap, firstKey, lastKey, remove. And add private helper methods: headMap, tailMap, fixAfterRemoving, transplantNode, getMax, getMin. Fix clear method

--- src/by/it/group351005/egorov/lesson12/MyRbMap.java
+++ src/by/it/group351005/egorov/lesson12/MyRbMap.java
@@ -30,11 +30,12 @@ public class MyRbMap implements SortedMap<Integer, String> {
     private TreeNode root = null;
     private int _size = 0;
 
-    private int getSize(TreeNode node) {
-        if (node == null) {
-            return 0;
+    private void clear(TreeNode node) {
+        if (node != null) {
+            clear(node.left);
+            clear(node.right);
+            node.left = node.right = node.parent = null;
         }
-        return Math.max(getSize(node.left), getSize(node.right)) + 1;
     }
 
     private void rotateRight(TreeNode node) {
@@ -113,7 +114,37 @@ public class MyRbMap implements SortedMap<Integer, String> {
         return get(node.key.compareTo(o) > 0 ? node.left : node.right, o);
     }
 
-    private void filterInsert(TreeNode newNode) {
+    private TreeNode getMin(TreeNode node) {
+        TreeNode min = node;
+        while (min.left != null) {
+            min = min.left;
+        }
+        return min;
+    }
+
+    private TreeNode getMax(TreeNode node) {
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
+    }
+
+    private void transplantNode(TreeNode toNode, TreeNode fromNode) {
+        if (toNode.parent == null) {
+            root = fromNode;
+        }
+        else if (toNode == toNode.parent.left) {
+            toNode.parent.left = fromNode;
+        }
+        else {
+            toNode.parent.right = fromNode;
+        }
+        if (fromNode != null) {
+            fromNode.parent = toNode.parent;
+        }
+    }
+
+    private void fixAfterInsert(TreeNode newNode) {
         if (newNode == root) {
             newNode.color = COLOR.BLACK;
             return;
@@ -159,6 +190,102 @@ public class MyRbMap implements SortedMap<Integer, String> {
         }
     }
 
+    private void fixAfterRemoving(TreeNode node) {
+        while (node != root && getColor(node) == COLOR.BLACK) {
+            TreeNode brother = null;
+            // if node is left child of parent
+            if (node == node.parent.left) {
+                brother = node.parent.right;
+                // if brother RED => make it BLACK, his parent BLACK and then leftRotate for parent
+                if (getColor(brother) == COLOR.RED) {
+                    brother.color = COLOR.BLACK;
+                    brother.parent.color = COLOR.RED;
+                    rotateLeft(brother.parent);
+                    brother = node.parent.right;
+                }
+                // if brother was black or became BLACK
+                // if two child of brother black, change brother color to RED:
+                // and parent color to BLACK, then fix parent balance
+                if (getColor(brother.left) == COLOR.BLACK && getColor(brother.right) == COLOR.BLACK) {
+                    brother.color = COLOR.RED;
+                    brother.parent.color = COLOR.BLACK;
+                    node = node.parent;
+                }
+                // if one of brother's child RED
+                else {
+                    // if left brother's child is RED:
+                    // make right brother's child RED, left brother's child BLACK
+                    // and then rightRotate for brother
+                    if (getColor(brother.left) == COLOR.RED) {
+                        brother.right.color = COLOR.RED;
+                        brother.left.color = COLOR.BLACK;
+                        rotateRight(brother);
+                        brother = node.parent.right;
+                    }
+                    // if right brother's child right is RED or became RED:
+                    // make right brother's child BLACK, then make brother color his parent's color,
+                    // parent color make BLACK and then right rotate for parent
+                    brother.right.color = COLOR.BLACK;
+                    brother.color = brother.parent.color;
+                    brother.parent.color = COLOR.BLACK;
+                    rotateLeft(brother.parent);
+                    node = root;
+                }
+            }
+            // if node is right child of parent
+            // make all symmetric
+            else {
+                brother = node.parent.right;
+                if (getColor(brother) == COLOR.RED) {
+                    brother.color = COLOR.BLACK;
+                    brother.parent.color = COLOR.RED;
+                    rotateRight(brother.parent);
+                    brother = node.parent.left;
+                }
+                if (getColor(brother.left) == COLOR.BLACK && getColor(brother.right) == COLOR.BLACK) {
+                    brother.color = COLOR.RED;
+                    brother.parent.color = COLOR.BLACK;
+                    node = node.parent;
+                }
+                else {
+                    if (getColor(brother.right) == COLOR.RED) {
+                        brother.left.color = COLOR.RED;
+                        brother.right.color = COLOR.BLACK;
+                        rotateLeft(brother);
+                        brother = node.parent.left;
+                    }
+                    brother.left.color = COLOR.BLACK;
+                    brother.color = brother.parent.color;
+                    brother.parent.color = COLOR.BLACK;
+                    rotateRight(brother.parent);
+                    node = root;
+                }
+            }
+        }
+    }
+
+    private void headMap(TreeNode node, Integer key, MyRbMap map) {
+        if (node == null) {
+            return;
+        }
+        if (node.key.compareTo(key) < 0) {
+            map.put(node.key, node.value);
+            headMap(node.right, key, map);
+        }
+        headMap(node.left, key, map);
+    }
+
+    private void tailMap(TreeNode node, Integer key, MyRbMap map) {
+        if (node == null) {
+            return;
+        }
+        if (node.key.compareTo(key) >= 0) {
+            map.put(node.key, node.value);
+            tailMap(node.left, key, map);
+        }
+        tailMap(node.right, key, map);
+    }
+
     private String inOrderTravel(TreeNode node) {
         if (node == null) {
             return "";
@@ -190,9 +317,9 @@ public class MyRbMap implements SortedMap<Integer, String> {
         return root == null;
     }
 
-    // TODO check if correct
     @Override
     public void clear() {
+        clear(root);
         root = null;
         _size = 0;
     }
@@ -228,14 +355,39 @@ public class MyRbMap implements SortedMap<Integer, String> {
         else {
             parent.left = curNode;
         }
-        filterInsert(curNode);
+        fixAfterInsert(curNode);
         _size++;
         return null;
     }
 
     @Override
     public String remove(Object o) {
-        return null;
+        TreeNode removedNode = findByKey(root, o);
+        if (removedNode == null) {
+            return null;
+        }
+        String removedValue = removedNode.value;
+        if (removedNode.left != null && removedNode.right != null) {
+            TreeNode minNode = getMin(removedNode.right);
+            removedNode.value = minNode.value;
+            removedNode.key = minNode.key;
+            removedNode = minNode;
+        }
+        TreeNode replacement = (removedNode.left != null) ? removedNode.left : removedNode.right;
+        if (replacement != null) {
+            transplantNode(removedNode, replacement);
+            if (removedNode.color == COLOR.BLACK) {
+                fixAfterRemoving(replacement);
+            }
+        } else {
+            transplantNode(removedNode,null);
+            if (getColor(removedNode) == COLOR.BLACK) {
+                fixAfterRemoving(removedNode.parent);
+            }
+        }
+        removedNode.left = removedNode.right = removedNode.parent = null;
+        _size--;
+        return removedValue;
     }
 
     @Override
@@ -254,6 +406,37 @@ public class MyRbMap implements SortedMap<Integer, String> {
         return findByValue(root, value) != null;
     }
 
+    @Override
+    public SortedMap<Integer, String> headMap(Integer key) {
+        MyRbMap map = new MyRbMap();
+        headMap(root, key,map);
+        return map;
+    }
+
+    @Override
+    public SortedMap<Integer, String> tailMap(Integer key) {
+        MyRbMap map = new MyRbMap();
+        tailMap(root, key, map);
+        return map;
+    }
+
+    @Override
+    public Integer firstKey() {
+        TreeNode node = getMin(root);
+        if (node == null) {
+            return null;
+        }
+        return node.key;
+    }
+
+    @Override
+    public Integer lastKey() {
+        TreeNode node = getMax(root);
+        if (node == null) {
+            return null;
+        }
+        return node.key;
+    }
 
     @Override
     public Comparator<? super Integer> comparator() {
@@ -262,26 +445,6 @@ public class MyRbMap implements SortedMap<Integer, String> {
 
     @Override
     public SortedMap<Integer, String> subMap(Integer integer, Integer k1) {
-        return null;
-    }
-
-    @Override
-    public SortedMap<Integer, String> headMap(Integer integer) {
-        return null;
-    }
-
-    @Override
-    public SortedMap<Integer, String> tailMap(Integer integer) {
-        return null;
-    }
-
-    @Override
-    public Integer firstKey() {
-        return null;
-    }
-
-    @Override
-    public Integer lastKey() {
         return null;
     }
 

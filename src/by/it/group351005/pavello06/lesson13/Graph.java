f1858455c1fff6bb92cd8f56d1a4689570cecd54
Pavel Halukha <pavelgaluha@gmail.com>
2024-11-03 12:52:52 +0300

    refact class Graph

--- src/by/it/group351005/pavello06/lesson13/Graph.java
+++ src/by/it/group351005/pavello06/lesson13/Graph.java
@@ -3,12 +3,14 @@ package by.it.group351005.pavello06.lesson13;
 import java.util.List;
 
 public class Graph<T> {
-    private class Vertex<T> {
+    public class Vertex {
         public T value;
-        public List<Vertex<T>> neighboringVertices;
+        public List<Vertex> neighboringVertices;
 
         public Vertex(T value) {
             this.value = value;
         }
     }
+
+    public List<Vertex> vertices;
 }

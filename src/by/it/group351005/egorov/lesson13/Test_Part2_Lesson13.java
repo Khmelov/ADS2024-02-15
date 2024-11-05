4be54044638cb328f15306da3c7a27653b220b9d
Aleeg0 <Sankafoxe@gmail.com>
2024-11-05 15:36:55 +0300

    test(lesson13): add taskA tests

--- src/by/it/group351005/egorov/lesson13/Test_Part2_Lesson13.java
+++ src/by/it/group351005/egorov/lesson13/Test_Part2_Lesson13.java
@@ -16,6 +16,23 @@ public class Test_Part2_Lesson13 extends HomeWork {
         run("0 -> 1, 0 -> 2, 0 -> 2, 1 -> 3, 1 -> 3, 2 -> 3", true).include("0 1 2 3");
         run("A -> B, A -> C, B -> D, C -> D", true).include("A B C D");
         run("A -> B, A -> C, B -> D, C -> D, A -> D", true).include("A B C D");
+        run("0 -> 2, 1 -> 3, 2 -> 3, 0 -> 1", true).include("0 1 2 3");
+        run("0 -> 1, 1 -> 2, 2 -> 3", true).include("0 1 2 3");
+        run("0 -> 2, 0 -> 3, 2 -> 3", true).include("0 2 3");
+        run("0 -> 1, 0 -> 3, 1 -> 2, 2 -> 3", true).include("0 1 2 3");
+        run("2 -> 3, 1 -> 3, 0 -> 1, 0 -> 2", true).include("0 1 2 3");
+        run("0 -> 1, 1 -> 2, 0 -> 3, 3 -> 2", true).include("0 1 3 2");
+        run("A -> B, A -> C, B -> D, C -> D", true).include("A B C D");
+        run("A -> B, B -> C, A -> D, D -> C", true).include("A B D C");
+        run("X -> Y, Y -> Z, X -> Z", true).include("X Y Z");
+        run("1 -> 3, 2 -> 3, 0 -> 1, 0 -> 2", true).include("0 1 2 3");
+        run("A -> B, A -> C, B -> D, C -> E, D -> E", true).include("A B D C E");
+        run("5 -> 2, 5 -> 0, 4 -> 0, 4 -> 1, 2 -> 3, 3 -> 1", true).include("5 4 2 3 1 0");
+        run("K -> L, L -> M, K -> M", true).include("K L M");
+        run("A -> C, B -> C, B -> D, A -> B", true).include("A B D C");
+        run("P -> Q, P -> R, Q -> S, R -> S", true).include("P Q R S");
+        run("0 -> 2, 0 -> 3, 1 -> 3, 2 -> 4, 3 -> 4", true).include("1 0 2 3 4");
+
         //Дополните эти тесты СВОИМИ более сложными примерами и проверьте их работоспособность.
         //Параметр метода run - это ввод. Параметр метода include - это вывод.
         //Общее число примеров должно быть не менее 20 (сейчас их 8).
@@ -26,6 +43,7 @@ public class Test_Part2_Lesson13 extends HomeWork {
         run("0 -> 1", true).include("no").exclude("yes");
         run("0 -> 1, 1 -> 2", true).include("no").exclude("yes");
         run("0 -> 1, 1 -> 2, 2 -> 0", true).include("yes").exclude("no");
+
         //Дополните эти тесты СВОИМИ более сложными примерами и проверьте их работоспособность.
         //Параметр метода run - это ввод. Параметр метода include - это вывод.
         //Общее число примеров должно быть не менее 12 (сейчас их 3).

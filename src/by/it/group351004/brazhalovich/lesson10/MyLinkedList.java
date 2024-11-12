3698193e7830bf19455b4d3625141385ec855d0e
Sunn4eS <sashabrazhalovich2005@gmail.com>
2024-11-12 13:16:06 +0300

    Lesson 10 A, B, C

--- src/by/it/group351004/brazhalovich/lesson10/MyLinkedList.java
+++ src/by/it/group351004/brazhalovich/lesson10/MyLinkedList.java
@@ -4,7 +4,8 @@ import java.util.Collection;
 import java.util.Deque;
 import java.util.Iterator;
 
-public class MyLinkedList<E> implements Deque<E> {
+public class
+MyLinkedList<E> implements Deque<E> {
 
     private class ListNode<E> {
         E data;

fa9a0e36f7e4ab37b938ba1d9662afcbe71d0289
Mihail <misabitno@gamil.com>
2024-11-15 17:34:33 +0300

    Commit lesson 11, 12

---
+++ src/by/it/group351005/bitno/lesson12/MyAvlMap.java
@@ -0,0 +1,79 @@
+package by.it.group351005.bitno.lesson12;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class MyAvlMap<Integer, String> implements Map<Integer, String> {
+
+    private class Node {
+        String value;
+        Integer key;
+        Node left;
+        Node right;
+        private Node (String value, Integer key, Node left, Node right){
+            this.value = value;
+            this.key = key;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    @Override
+    public int size() {return 0;}
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public String get(Object key) {
+        return null;
+    }
+
+    @Override
+    public String put(Integer key, String value) {
+        return null;
+    }
+
+    @Override
+    public String remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends String> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<String> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<Integer, String>> entrySet() {
+        return null;
+    }
+}
---
+++ src/by/it/group351005/bitno/lesson12/MyRbMap.java
@@ -0,0 +1,95 @@
+package by.it.group351005.bitno.lesson12;
+
+import java.util.*;
+
+public class MyRbMap<E> implements SortedMap<Integer, String> {
+    @Override
+    public Comparator<? super Integer> comparator() {
+        return null;
+    }
+
+    @Override
+    public SortedMap<Integer, String> subMap(Integer fromKey, Integer toKey) {
+        return null;
+    }
+
+    @Override
+    public SortedMap<Integer, String> headMap(Integer toKey) {
+        return null;
+    }
+
+    @Override
+    public SortedMap<Integer, String> tailMap(Integer fromKey) {
+        return null;
+    }
+
+    @Override
+    public Integer firstKey() {
+        return null;
+    }
+
+    @Override
+    public Integer lastKey() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public String get(Object key) {
+        return null;
+    }
+
+    @Override
+    public String put(Integer key, String value) {
+        return null;
+    }
+
+    @Override
+    public String remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends String> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<String> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<Integer, String>> entrySet() {
+        return null;
+    }
+}

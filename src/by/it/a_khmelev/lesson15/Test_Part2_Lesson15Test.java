2796d6fc8b9203f3d83e86295ee24ca372b527b7
akhmelev <akhmelev@gmail.com>
2024-11-20 12:53:04 +0300

    fix lesson15 (lazy walk)

--- src/by/it/a_khmelev/lesson15/Test_Part2_Lesson15Test.java
+++ src/by/it/a_khmelev/lesson15/Test_Part2_Lesson15Test.java
@@ -1,7 +1,6 @@
 package by.it.a_khmelev.lesson15;
 
 import by.it.HomeWork;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -16,15 +15,10 @@ public class Test_Part2_Lesson15Test extends HomeWork {
 
     private static List<String> samples;
 
-    @BeforeClass
-    public static void init() throws IOException {
-        samples = samples();
-    }
-
     @Test(timeout = 5000) //можно изменить под свою производительность
     public void testSourceScannerA() {
         HomeWork run = run("");
-        for (String sample : samples) {
+        for (String sample : lazyWalk()) {
             run.include(sample);
         }
     }
@@ -32,7 +26,7 @@ public class Test_Part2_Lesson15Test extends HomeWork {
     @Test(timeout = 5000) //можно изменить под свою производительность
     public void testSourceScannerB() {
         HomeWork run = run("");
-        for (String sample : samples) {
+        for (String sample : lazyWalk()) {
             run.include(sample);
         }
     }
@@ -43,29 +37,33 @@ public class Test_Part2_Lesson15Test extends HomeWork {
         run("").include("FiboA.java");
     }
 
-    private static List<String> samples() throws IOException {
-        ArrayList<String> files = new ArrayList<>();
-        Path root = Path.of(System.getProperty("user.dir")
-                            + File.separator + "src" + File.separator);
-        try (var walk = Files.walk(root)) {
-            walk.forEach(
-                    p -> {
-                        if (p.toString().endsWith(".java")) {
-                            try {
-                                String s = Files.readString(p);
-                                if (!s.contains("@Test") && !s.contains("org.junit.Test")) {
-                                    files.add(root.relativize(p).toString());
-                                }
-                            } catch (IOException e) {
-                                if (System.currentTimeMillis() < 0) {
-                                    System.err.println(p);
+    private static List<String> lazyWalk() {
+        if (samples == null) {
+            samples = new ArrayList<>();
+            Path root = Path.of(System.getProperty("user.dir")
+                                + File.separator + "src" + File.separator);
+            try (var walk = Files.walk(root)) {
+                walk.forEach(
+                        p -> {
+                            if (p.toString().endsWith(".java")) {
+                                try {
+                                    String s = Files.readString(p);
+                                    if (!s.contains("@Test") && !s.contains("org.junit.Test")) {
+                                        samples.add(root.relativize(p).toString());
+                                    }
+                                } catch (IOException e) {
+                                    if (System.currentTimeMillis() < 0) {
+                                        System.err.println(p);
+                                    }
                                 }
                             }
                         }
-                    }
-            );
+                );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-        return files;
+        return samples;
     }
 
 }

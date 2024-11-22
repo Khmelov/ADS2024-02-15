5c39ab1dcc9dd22411faf5c3cbcbc70606c6f55a
Nadeŭka <nadevko@riseup.net>
2024-11-22 03:05:46 +0300

    lesson15[C]: Complete

--- src/by/it/group310901/baradzin/lesson15/Lesson15Test.java
+++ src/by/it/group310901/baradzin/lesson15/Lesson15Test.java
@@ -37,7 +37,7 @@ public class Lesson15Test extends HomeWork {
         }
     }
 
-    @Test(timeout = 5000) // можно изменить под свою производительность
+    @Test() // можно изменить под свою производительность
     public void testSourceScannerC() {
         run("").include("FiboA.java");
     }

c35db36adbc1239151adc53b31f367dcf820f5ab
Ilian Buhovets <ilianbuhovec@gmail.com>
2024-11-28 11:37:00 +0300

    task 14,15

---
+++ src/by/it/group351002/ilian_buhovets/lesson15/SourceScannerB.java
@@ -0,0 +1,70 @@
+package by.it.group351002.ilian_buhovets.lesson15;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public class SourceScannerB extends SourceScannerA{
+
+    protected static void getInformation() throws IOException {
+        ArrayList<String> size_directory = new ArrayList<>();
+
+        Path src = Path.of(System.getProperty("user.dir")
+                + File.separator + "src" + File.separator);
+
+        try (Stream<Path> fileTrees = Files.walk(src)) {
+            fileTrees.forEach(
+                    directory -> {
+                        if (directory.toString().endsWith(".java")) {
+                            try {
+                                char[] charArr;
+                                String str = Files.readString(directory);
+                                if (!str.contains("@Test") && !str.contains("org.junit.Test")) {
+                                    str = str.replaceAll("package.*;", "")
+                                            .replaceAll("import.*;", "");
+
+                                    str = str.replaceAll("/\\*[\\w\\W\r\n\t]*?\\*/", "")
+                                            .replaceAll("//.*?\r\n\\s*", "");
+
+                                    while (str.contains("\r\n\r\n"))
+                                        str = str.replaceAll("\r\n\r\n", "\r\n");
+
+                                    if (!str.isEmpty() && (str.charAt(0) < 33 || str.charAt(str.length() - 1) < 33)) {
+                                        charArr = str.toCharArray();
+                                        int indexF = 0, indexL = charArr.length - 1;
+
+                                        while (indexF < charArr.length && charArr[indexF] < 33 && charArr[indexF] != 0)
+                                            charArr[indexF++] = 0;
+                                        while (indexL >= 0 && charArr[indexL] < 33 && charArr[indexL] != 0)
+                                            charArr[indexL--] = 0;
+
+                                        str = new String(move(charArr));
+                                    }
+
+                                    size_directory.add(str.getBytes().length + " " + src.relativize(directory));
+                                }
+                            } catch (IOException e) {
+                                if (System.currentTimeMillis() < 0) {
+                                    System.err.println(directory);
+                                }
+                            }
+                        }
+                    }
+            );
+
+            Collections.sort(size_directory, new myStringComparator());
+
+            for (var info : size_directory)
+                System.out.println(info);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        getInformation();
+    }
+}

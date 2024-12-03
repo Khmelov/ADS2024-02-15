466361f62e310b8728de42009a0d8c5d25c59e13
cycyclist <valentinsarycev@gmail.com>
2024-12-03 13:28:54 +0300

    Lesson 15

--- src/by/it/group351001/v_sarychev/lesson15/Test_Part2_Lesson15Test.java
+++ src/by/it/group351001/v_sarychev/lesson15/Test_Part2_Lesson15Test.java
@@ -15,7 +15,7 @@ public class Test_Part2_Lesson15Test extends HomeWork {
 
     private static List<String> samples;
 
-    @Test(timeout = 5000) //можно изменить под свою производительность
+    @Test(timeout = 10000) //можно изменить под свою производительность
     public void testSourceScannerA() {
         HomeWork run = run("");
         for (String sample : lazyWalk()) {
@@ -32,7 +32,7 @@ public class Test_Part2_Lesson15Test extends HomeWork {
     }
 
 
-    @Test(timeout = 5000) //можно изменить под свою производительность
+    @Test(timeout = 10000) //можно изменить под свою производительность
     public void testSourceScannerC() {
         run("").include("FiboA.java");
     }

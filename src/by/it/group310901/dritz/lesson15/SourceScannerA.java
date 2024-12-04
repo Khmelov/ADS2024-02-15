f983951d68485738d38e5f2a296cb471dc6bcb1d
BebrikMMD <ppeshki@bk.ru>
2024-12-04 09:23:24 +0300

    lesson15

---
+++ src/by/it/group310901/dritz/lesson15/SourceScannerA.java
@@ -0,0 +1,54 @@
+package by.it.group310901.dritz.lesson15;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.AbstractMap;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+
+public class SourceScannerA {
+    public static void main(String[] args) {
+        var root = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        try {
+            Files.walk(Path.of(root))
+                    .filter(path -> path.toString().endsWith(".java"))
+                    .map(path -> {
+                        try {
+                            var content = Files.readString(path);
+                            if (content.contains("@Test") || content.contains("org.junit.Test"))
+                                return null;
+                            content = processContent(content);
+                            return content.isEmpty() ? null
+                                    : new AbstractMap.SimpleEntry<>(path, content.getBytes().length);
+                        } catch (IOException e) {
+                            System.err.println("Error reading file: " + path);
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .sorted(Comparator.comparingInt(Map.Entry<Path, Integer>::getValue)
+                            .thenComparing(Map.Entry::getKey))
+                    .forEach(entry -> System.out
+                            .println(entry.getValue() + " " + Path.of(root).relativize(entry.getKey())));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String processContent(String content) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String line : content.split("\n")) {
+            if (line.startsWith("package") || line.startsWith("import"))
+                continue;
+            line = line.replaceAll("^[\\x00-\\x1F]+|[\\x00-\\x1F]+$", "");
+            sb.append(line).append("\n");
+        }
+
+        return sb.toString().trim();
+    }
+}
